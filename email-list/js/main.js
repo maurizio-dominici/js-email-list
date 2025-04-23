b0ec735386e3c437e3157230e74c0655fbd4bee6
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-23 16:41:03 +0200

    add list

--- email-list/js/main.js
+++ email-list/js/main.js
@@ -1,6 +1,6 @@
 // ^ RACCOLTA DATI
 const emailList = document.getElementById("list-group-email");
-
+let textList = "";
 // console.log(emailList);
 
 // ^ SVOLGIMENTO
@@ -11,7 +11,26 @@ let email = [];
 for (let i = 0; i < 10; i++) {
   axios.get(apiUri).then((response) => {
     const currentEmail = response.data.response;
+
     email.push(currentEmail);
+
+    if (email.length === 10) {
+      emailList.innerHTML = `
+            <ul class="list-group">
+             <li class="list-group-item">${email[0]}</li>
+             <li class="list-group-item">${email[1]}</li>
+             <li class="list-group-item">${email[2]}</li>
+             <li class="list-group-item">${email[3]}</li>
+             <li class="list-group-item">${email[4]}</li>
+             <li class="list-group-item">${email[5]}</li>
+             <li class="list-group-item">${email[6]}</li>
+             <li class="list-group-item">${email[7]}</li>
+             <li class="list-group-item">${email[8]}</li>
+             <li class="list-group-item">${email[9]}</li>
+            </ul>
+        `;
+
+      //   console.log(email);
+    }
   });
 }
-console.log(email);
